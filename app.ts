959387fc0f83faa28de7787faf56f40f3cb76fbb
fshoukat111 <fshoukat111@gmail.com>
2022-02-24 23:37:14 +0500

    'modification'

--- app.ts
+++ app.ts
@@ -2,17 +2,19 @@ import express from "express";
 import { category,portfolio,user } from "@app/routes";
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser";
+import cors from "cors";
 
 export  const app = express();
 const errorMiddleware = require("@app/middleware/errors");
 
 // Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({ path: "app/.env" });
+    require("dotenv").config({ path: ".env" });
 }
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/v1", category);
 app.use("/api/v1", portfolio);
