70def70052c4edd62ecafdad29d625174f6ad0f1
Fahad <fahad.shokat111@gmail.com>
2022-02-25 19:39:35 +0500

    some_modification

--- app/routes/UserRoutes/user.routes.ts
+++ app/routes/UserRoutes/user.routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { registerUser,loginUser,logout } from "@app/controllers/UserController/user.controller";
+import { registerUser,loginUser,logout, getAllUsers } from "@app/controllers/UserController/user.controller";
 
 //post single Portfolio
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/users").get(getAllUsers);
 router.route("/logout").post(logout);
 module.exports = router;
