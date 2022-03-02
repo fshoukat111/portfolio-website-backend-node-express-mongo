db9d79e2a5396b1dfb5b29b927a44d33f3f4b9d8
fshoukat111 <fshoukat111@gmail.com>
2022-03-02 10:45:26 +0500

    'te'

--- app/config/database.ts
+++ app/config/database.ts
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 import { ConnectOptions } from "mongodb";
 
-const connectDataBase = () => {
+export const connectDataBase = () => {
     mongoose.connect('mongodb://localhost:27017/portfolio', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }as ConnectOptions).then((data:any) => {
     })
-}
-export {
-    connectDataBase
 }
