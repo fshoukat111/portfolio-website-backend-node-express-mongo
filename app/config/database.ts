4ec9d74d43cfbb80240dd05276d41dc69f6e4f8f
fshoukat111 <fshoukat111@gmail.com>
2022-03-09 22:00:29 +0500

    'minor-changes'

--- app/config/database.ts
+++ app/config/database.ts
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 import { ConnectOptions } from "mongodb";
 
 export const connectDataBase = () => {
-    mongoose.connect('mongodb://localhost:27017/portfolio', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }as ConnectOptions).then((data:any) => {
-    });
-    // mongoose.connect(`${process.env.MONGO_URI}` || 'localhost:27017/portfolio', {
+    // mongoose.connect('mongodb://localhost:27017/portfolio', {
     //     useNewUrlParser: true,
     //     useUnifiedTopology: true,
     // }as ConnectOptions).then((data:any) => {
     // });
+    mongoose.connect(`${process.env.MONGO_URI}` || 'localhost:27017/portfolio', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }as ConnectOptions).then((data:any) => {
+    });
 }
